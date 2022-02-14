e77ffd1a1cf64831361fdcc8b85c4e4a221bb7a4
mich-viv <97334936+mich-viv@users.noreply.github.com>
2022-02-14 12:35:47 +0100

    Update attachment.php

--- backend/attachment.php
+++ backend/attachment.php
@@ -1,5 +1,7 @@
 <?php
 
+//this script will create a list of all the attachmente already present in the server 
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Max-Age: 3628800');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');  
@@ -7,11 +9,8 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 $fileList = glob('upload/*');
 
 foreach($fileList as $filename){
-	
 	if(is_file($filename)){
 		echo str_replace("upload/","",$filename)."~~~";
 	}
- 
-}
-
+}//foreach
 ?>
