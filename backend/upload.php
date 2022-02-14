a8f3f9afe3c682bfd5f84e38b238bfbae1788cfe
mich-viv <97334936+mich-viv@users.noreply.github.com>
2022-02-14 12:34:58 +0100

    Update upload.php

--- backend/upload.php
+++ backend/upload.php
@@ -1,17 +1,13 @@
 <?php
+//this script is handling hte upload of the document that the admin wants to send as an attachment to the server
 
-    header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Origin: *');  
 
-    if (move_uploaded_file($_FILES["file"]["tmp_name"], "upload/".$_FILES['file']['name'])) {
+if (move_uploaded_file($_FILES["file"]["tmp_name"], "upload/".$_FILES['file']['name'])) {
+    echo "OK";
+    exit;
+}
 
-        echo "OK";
-
-        exit;
-
-    }
-
-    echo "FAIL";
-
-  
+echo "FAIL";
 
 ?>
